915b346f5838a43ba245301c112048f61bb37b80
Tedyang2003 <awsy2003@gmail.com>
2025-01-08 16:51:51 +0800

    Introduce 3 berts

--- models/distilbart-cnn-12-6/model_non_pipeline.py
+++ models/distilbart-cnn-12-6/model_non_pipeline.py
@@ -10,12 +10,22 @@ class TritonPythonModel:
     
     def initialize(self, args:Dict[str, str]) -> None:
         self.logger = pb_utils.Logger
+        torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float324
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         path: str = os.path.join(args['model_repository'], args['model_version'])
         # path: str = 'sshleifer/distilbart-cnn-12-6'
-        self.summariser = pipeline('summarization', model=path, device=0)
 
+        self.model = BartForConditionalGeneration.from_pretrained(
+            path,
+            torch_dtype = torch_dtype,
+        )
+        self.model.to(self.device)
+        
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            path
+        )
+        
                 
     def execute(self, requests):
         responses = []
@@ -27,14 +37,22 @@ class TritonPythonModel:
             text_input = pb_utils.get_input_tensor_by_name(request, "INPUT")
             text = text_input.as_numpy()[0].decode("utf-8")  # Assuming the text is encoded as bytes
 
-            output = self.summariser(text)
 
-            self.logger.log_info(f"Summary: {output}...")
+            inputs = self.tokenizer([text], max_length=1024, truncation=True, return_tensors="pt").to(self.device)
+            
+
+            # You can adjust max_length and min length as needed
+            summary_ids = self.model.generate(
+                inputs["input_ids"]
+            )
+            # Decode the generated summary
+            summary_text = self.tokenizer.batch_decode(summary_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+            self.logger.log_info(f"Summary: {summary_text}...")
             
             # Prepare the inference response
             inference_response = pb_utils.InferenceResponse(
                 output_tensors=[
-                    pb_utils.Tensor("OUTPUT", np.array([output[0]['summary_text'].encode()]))  # Encode back to bytes
+                    pb_utils.Tensor("OUTPUT", np.array([summary_text.encode()]))  # Encode back to bytes
                 ]
             )
 
